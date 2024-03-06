017d65727fb8c81f29ce455110af67306c3b8bed
Mnimaya <mnimayaaa@gmail.com~>
2024-03-06 08:42:00 +0300

    fix: handler menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,41 +12,41 @@ function Header() {
   const path = location.pathname;
   const isOpen = useAppSelector((state) => state.header.isOpen);
   const dispatch = useAppDispatch();
-  const handlerMobileMenu = () => {
-    dispatch(toggle(!isOpen));
+  const handlerMobileMenu = (isOpen: boolean) => {
+    dispatch(toggle(isOpen));
     dispatch(handlerSearch(false));
   };
 
   return (
     <header className={`${styles.header} ${isOpen && styles.headerDark} ${path !== '/' && styles.headerOpacity}`}>
       <div className={`container ${styles.container}`}>
-        <Link onClick={handlerMobileMenu} to="/" className={styles.logoLink}>
+        <Link onClick={() => handlerMobileMenu(false)} to="/" className={styles.logoLink}>
           <Logo></Logo>
         </Link>
         <div className={`${styles.navContainer} ${isOpen ? styles.active : ''}`}>
           <nav className={`${styles.nav} navigation`}>
             <ul>
               <li>
-                <Link onClick={handlerMobileMenu} to="catalog/films">
+                <Link onClick={() => handlerMobileMenu(false)} to="catalog/films">
                   Фильмы
                 </Link>
               </li>
               <li>
-                <Link onClick={handlerMobileMenu} to="catalog/series">
+                <Link onClick={() => handlerMobileMenu(false)} to="catalog/series">
                   Сериалы
                 </Link>
               </li>
               <li>
-                <Link onClick={handlerMobileMenu} to="catalog/mult">
+                <Link onClick={() => handlerMobileMenu(false)} to="catalog/mult">
                   Мультфильмы
                 </Link>
               </li>
             </ul>
           </nav>
           <Search />
-          <LinkClient secondaryClass="client" />
+          <LinkClient onClick={() => handlerMobileMenu(false)} secondaryClass="client" />
         </div>
-        <button className={`${styles.buttonMenu} ${isOpen ? styles.active : ''}`} onClick={() => handlerMobileMenu()} type="button" title="Открыть или закрыть меню.">
+        <button className={`${styles.buttonMenu} ${isOpen ? styles.active : ''}`} onClick={() => handlerMobileMenu(!isOpen)} type="button" title="Открыть или закрыть меню.">
           <span></span>
         </button>
       </div>
